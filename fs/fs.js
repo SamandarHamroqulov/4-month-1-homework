4054b37890e93b38916673d08af2d1cd8cc4d5e6
SamandarHamroqulov <s4410206@gmail.com>
2025-12-23 14:42:06 +0500

    Initial commit

---
+++ fs/fs.js
@@ -0,0 +1,52 @@
+const fs  = require("fs");
+// Fayl yaratish
+// fs.writeFileSync('.fs/hello.txt', 'Salom, Dunyo!');
+
+// // Faylni o'qish
+
+
+
+
+// const data = fs.readFileSync('./fs/hello.txt', 'utf8');
+// console.log(data);
+
+// // Faylga ma'lumot qo'shish
+
+
+
+// fs.appendFileSync("./fs/hello.txt", "  Men appendFileSync orqali qo'shildim")
+
+// // Faylni o'chrish
+
+
+
+
+// fs.writeFileSync("./fs/deleted.txt" , "men unlink bilan o'chirilaman" )
+// fs.unlinkSync("./fs/deleted.txt")
+
+
+
+// Papka yaratish va o'chirish
+
+// fs.mkdirSync("path")
+
+// fs.rmdirSync("path")
+
+
+// Fayl mavjudligini tekshirish
+
+// let acces = fs.accessSync("./fs/fs.js")
+// console.log(acces);
+
+
+// Fayl nomini o'zgartirish
+
+// fs.renameSync("salom.t", "./fs/salom.txt");
+
+// Fayl ma'lumotlarini olish
+
+
+// const stat = fs.statSync("./fs/salom.txt")
+// console.log(stat);
+
+
